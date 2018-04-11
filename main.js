a49e75e367236ad8d22f788a16f4db6efa5855d1
Michaela Seewald <michaela.seewald@student.uibk.ac.at>
2018-04-11 17:25:52 +0200

    javascript 101 bis datentypen

---
+++ main.js
@@ -0,0 +1,27 @@
+// alert("Hello world");
+
+let nachricht = "Hallo Welt";
+// alert(nachricht);
+nachricht = "neuer text";
+// alert(nachricht)
+console.log(nachricht);
+const lnkColor = "#ff0000";
+let eine_variable = "test";
+console.log("Variable: eine_variable: ", eine_variable);
+let highScore = 759944;
+console.log("Highscore ist: ", highScore / 20);
+let meineNachricht = "test1";
+console.log(meineNachricht);
+meineNachricht = 'test2';
+console.log(meineNachricht);
+meineNachricht = `Ein mehr
+und ein
+ganz langer
+text
+und aus`;
+console.log(meineNachricht);
+
+let isOver18 = true;
+console.log("über 18?", isOver18);
+let age = 17;
+console.log("age über 18", age > 18);
